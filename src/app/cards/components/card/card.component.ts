f27dea256b666d6ea58f3a89e3c3c1c45d8292f8
Jagpreet Singh <jagpreet.singh@capco.com>
2020-08-27 22:52:06 -0400

    cleanup

--- src/app/cards/components/card/card.component.ts
+++ src/app/cards/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardItem } from '../../../models';
 
 @Component({
@@ -6,12 +6,8 @@ import { CardItem } from '../../../models';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() item: CardItem;
-
-  constructor() {}
-
-  ngOnInit(): void {}
 
   get cssClass() {
     return this.item.type.toLowerCase();
