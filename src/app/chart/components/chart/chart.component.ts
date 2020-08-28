f405f6f85beb924cc5f3c2dd9ade7d65f22493ab
Jagpreet Singh <jagpreet.singh@capco.com>
2020-08-28 14:26:04 -0400

    label update issue fix

--- src/app/chart/components/chart/chart.component.ts
+++ src/app/chart/components/chart/chart.component.ts
@@ -1,20 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { BaseChartDirective, Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
+export class ChartComponent implements OnChanges {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
   @Input() barChartData: ChartDataSets[];
   @Input() country: string;
 
   barChartOptions: ChartOptions = {
     responsive: true
   };
-  barChartLabels: Label[] = ['Globally'];
+  barChartLabels: Label[] = [''];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartColors: Color[] = [
@@ -22,4 +24,12 @@ export class ChartComponent {
     { backgroundColor: 'rgba(0, 255, 0, 0.5)' },
     { backgroundColor: 'rgba(255, 0, 0, 0.5)' }
   ];
+
+  /** Fix for Chart label update issue */
+  ngOnChanges() {
+    this.barChartLabels.length = 0;
+    this.barChartLabels.push(
+      this.country ? `Current state in ${this.country}` : ''
+    );
+  }
 }
