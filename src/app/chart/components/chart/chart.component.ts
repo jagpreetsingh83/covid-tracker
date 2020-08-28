6629dad29cdc7703b4f6df0181c678dfa7d9d8eb
Jagpreet Singh <jagpreet.singh@capco.com>
2020-08-28 12:53:28 -0400

    minor cleanup

--- src/app/chart/components/chart/chart.component.ts
+++ src/app/chart/components/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
@@ -11,23 +11,15 @@ export class ChartComponent {
   @Input() barChartData: ChartDataSets[];
   @Input() country: string;
 
-  public barChartOptions: ChartOptions = {
+  barChartOptions: ChartOptions = {
     responsive: true
   };
-  public barChartLabels: Label[] = ['2006'];
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
-  public barChartPlugins = [];
-
-  get title() {
-    return this.country ? `Current state in ${this.country}` : '';
-  }
-
-  colorScheme = {
-    domain: [
-      'rgba(0, 0, 255, 0.5)',
-      'rgba(0, 255, 0, 0.5)',
-      'rgba(255, 0, 0, 0.5)'
-    ]
-  };
+  barChartLabels: Label[] = ['Globally'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartColors: Color[] = [
+    { backgroundColor: 'rgba(0, 0, 255, 0.5)' },
+    { backgroundColor: 'rgba(0, 255, 0, 0.5)' },
+    { backgroundColor: 'rgba(255, 0, 0, 0.5)' }
+  ];
 }
